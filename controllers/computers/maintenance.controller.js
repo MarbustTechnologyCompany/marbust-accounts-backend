b75b0a0fb9181f7948e234ac306fdab392b1c3a2
MarAntBQ <github@marantbq.dev>
2025-01-16 23:33:49 -0500

    Agregar función para crear un nuevo equipo con verificación de existencia

--- controllers/computers/maintenance.controller.js
+++ controllers/computers/maintenance.controller.js
@@ -19,3 +19,34 @@ exports.getMyComputers = async (req, res) => {
         res.status(500).json({ error: 'Error procesando la solicitud', serverReport: error});
     }
 }
+
+exports.createComputer = async (req, res) => {
+    // Get data from request body and userId from token
+    const { name, computerType } = req.body;
+
+    // Search if computer already exists
+    const computerExists = await Computer.findOne({
+        where: {
+            name,
+            computerType: computerType,
+            userId: req.userId
+        }
+    });
+
+    if (computerExists) {
+        return res.status(400).json({ error: 'Ya existe un equipo con ese nombre y tipo' });
+    }
+
+    try {
+        const newComputer = await Computer.create({
+            name,
+            userId: req.userId,
+            computerType
+        });
+
+        res.status(201).json(newComputer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error procesando la solicitud', serverReport: error});
+    }
+}
